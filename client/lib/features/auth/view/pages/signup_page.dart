29c80da0d30e9e701b67d4bbb3330868860bae53
satyam-kr03 <b22236@students.iitmandi.ac.in>
2024-09-15 15:42:58 +0530

    signup page

--- client/lib/features/auth/view/pages/signup_page.dart
+++ client/lib/features/auth/view/pages/signup_page.dart
@@ -1,3 +1,5 @@
+import 'package:client/core/theme/app_pallete.dart';
+import 'package:client/features/auth/view/widgets/auth_gradient_button.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/custom_field.dart';
@@ -10,20 +12,75 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    super.dispose();
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    formKey.currentState!.validate();
+}
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Scaffold(
         appBar: AppBar(),
-        body: Column(
-          children: [
-            const Text(
-              'Sign Up',
-              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-            ),
-            CustomField(),
-          ],
+        body: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Sign Up.',
+                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 30),
+              CustomField(
+                controller: nameController,
+                hintText: 'Name',
+              ),
+              const SizedBox(height: 15),
+              CustomField(
+                controller: emailController,
+                hintText: 'Email',
+              ),
+              const SizedBox(height: 15),
+              CustomField(
+                controller: passwordController,
+                isObscureText: true,
+                hintText: 'Password',
+              ),
+              const SizedBox(height: 20),
+              AuthGradientButton(
+                buttonText: 'Sign up',
+                onTap: () async {},
+              ),
+              RichText(
+                  text: TextSpan(
+                      text: 'Already have an account? ',
+                      style: Theme.of(context).textTheme.titleMedium,
+                      children: [
+                    TextSpan(
+                        text: 'Sign In',
+                        style: TextStyle(
+                          color: Pallete.gradient2,
+                          fontWeight: FontWeight.bold,
+                        )
+                    )
+                  ]
+                  )
+              )
+            ],
+          ),
         ),
-
+      ),
     );
   }
 }
--- client/lib/features/auth/view/widgets/custom_field.dart
+++ client/lib/features/auth/view/widgets/custom_field.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 
 class CustomField extends StatelessWidget {
-  const CustomField({super.key});
+  final String hintText;
+  final TextEditingController controller;
+  final bool isObscureText;
+  const CustomField({super.key, required this.hintText, required this.controller, this.isObscureText = false});
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField();
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hintText,
+      ),
+      validator: (val){
+        if(val!.trim().isEmpty) {
+          return "$hintText is missing";
+        }
+        return null;
+      },
+      obscureText: isObscureText,
+    );
   }
 }
